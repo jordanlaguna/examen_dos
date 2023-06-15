096d21c253594e0ec07c152f8f8dc7589cc0e99e
jordanlaguna <126918343+jordanlaguna@users.noreply.github.com>
2023-06-15 16:11:05 -0600

    first commit

---
+++ src/componets/head.jsx
@@ -0,0 +1,19 @@
+const Page_head = ({}) => {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "#353535",
+          padding: "10px",
+          textAlign: "left",
+          marginTop: "-3.5%",
+        }}
+      >
+        <h1 style={{ margin: "0", fontSize: "24px", color: "black" }}>
+          ChatM&J
+        </h1>
+      </div>
+    </>
+  );
+};
+export default Page_head;
