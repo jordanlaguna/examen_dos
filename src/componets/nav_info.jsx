2cddb369b2851f8ab89b0f04d69b62c830ef066c
Melany Mendoza Vargas <melanymendoza2001@gmail.com>
2023-06-17 14:19:59 -0600

    create text nav info

--- src/componets/nav_info.jsx
+++ src/componets/nav_info.jsx
@@ -1,10 +1,17 @@
-const Page_nav = ({}) => {
+const Page_nav = ({ }) => {
     return (
-      <>
-        <nav className="navPage">
-          <h1 className="tittle">hola mundooooo</h1>
-        </nav>
-      </>
+        <>
+            <nav className="navPage">
+                <h1 className="title-nav">Generador de Preguntas</h1>
+                <h2 className="info-nav">
+                    Nuestro sitio web, creado por Melany Mendoza junto con Jordan Laguna, <br />
+                    funciona como una aplicación generadora de contenido utilizando la API GPT. <br />
+                    Esta funcionalidad es útil para aquellos que deseen obtener una <br />
+                    visión general o profundizar en un tema en particular <br />
+                    haciéndole preguntas y generando respuesta utilizando inteligencia artificial.
+                </h2>
+            </nav>
+        </>
     );
-  };
-  export default Page_nav;
+};
+export default Page_nav;
