ed8e9647dd6a52f7d0903fcc86e44da40bb56132
jordanlaguna <126918343+jordanlaguna@users.noreply.github.com>
2023-06-17 21:28:54 -0600

    update

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const config = {
+  openaiApiKey: "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",
+};
+export default config;
--- src/config.jsx
+++
@@ -1,4 +0,0 @@
-const config = {
-  openaiApiKey: "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",
-};
-export default config;
